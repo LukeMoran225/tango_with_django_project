04d4b03dfc3207de9a1e9370c68ee1c4c11893a3
Luke Moran <2259458m@student.gla.ac.uk>
2020-01-22 23:43:02 +0000

    Completed 6.2. Categories listed on index page.

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
+
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to in the template.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our bold message).
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -15,4 +26,3 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Luke Moran'}
     return render(request, 'rango/about.html', context=context_dict)
-
